e2c35a2a4fa82835ad095ebc56a77e7b2c81fe89
Alena Buiko <abuiko.ma@gmail.com>
2021-11-01 19:47:46 -0400

    align inputs, button, img

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
 import './App.css'
-import React from 'react'
+import React, { Component } from 'react'
 import Img from './assets/meme.jpeg'
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <h1>Meme Generator</h1>
-      </header>
-      <main className="img-container">
-        <img className="img" src={Img} alt="random " />
-      </main>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      topText: "",
+      bottomText: "",
+      randomImg: ""
+    }
+  }
+  handleChange = (event) => {
+    const { name, value } = event.target
+    this.setState({
+      [name]: value
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Meme Generator</h1>
+        </header>
+        <main className="img-container">
+          <img className="img" src={Img} alt="random " />
+        </main>
+        <form>
+          <div className="form-container">
+            <input
+              type="text"
+              name="topText"
+              value={this.state.topText}
+              placeholder="Top Text"
+              onChange={this.handleChange}
+            />
+            <input
+              type="text"
+              name="topText"
+              value={this.state.bottomText}
+              placeholder="Bottom Text"
+              onChange={this.handleChange}
+            />
+            <button>Generate</button>
+          </div>
+
+        </form>
+      </div>
+    )
+  }
+
 }
 
 export default App;
